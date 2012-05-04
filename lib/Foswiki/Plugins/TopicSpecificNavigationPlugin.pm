397a700d16a3a61808d6c3f6ecd76919fd2ec81a
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/TopicSpecificNavigationPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/TopicSpecificNavigationPlugin.pm
+++ lib/Foswiki/Plugins/TopicSpecificNavigationPlugin.pm
@@ -1,6 +1,6 @@
 #!/usr/local/bin/perl -wI.
 #
-# This script Copyright (c) 2008 Impressive.media 
+# This script Copyright (c) 2008 Impressive.media
 # and distributed under the GPL (see below)
 #
 # Based on parts of GenPDF, which has several sources and authors
@@ -14,11 +14,13 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
 
 # =========================
-package Foswiki::Plugins::TopicSpecificNavigationPlugin;    # change the package name and $pluginName!!!
+package Foswiki::Plugins::TopicSpecificNavigationPlugin
+  ;    # change the package name and $pluginName!!!
+
 # =========================
 # Always use strict to enforce variable scoping
 use strict;
@@ -27,10 +29,11 @@ use Digest::MD5 qw(md5_hex);
 use Foswiki::Plugins::DBConnectorPlugin;
 use Error qw(:try);
 
-
 # $VERSION is referred to by Foswiki, and is the only global variable that
 # *must* exist in this package.
-use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION $debug $pluginName $NO_PREFS_IN_TOPIC );
+use vars
+  qw( $VERSION $RELEASE $SHORTDESCRIPTION $debug $pluginName $NO_PREFS_IN_TOPIC );
+
 # This should always be $Rev: 12445$ so that Foswiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
 # you should leave it alone.
@@ -43,48 +46,59 @@ $RELEASE = '0.6';
 
 # Short description of this plugin
 # One line description, is shown in the %FoswikiWEB%.TextFormattingRules topic:
-$SHORTDESCRIPTION = 'This plugins introduces a new navigation type for each topic: It should be used to place things like related links, further readups etc.';
+$SHORTDESCRIPTION =
+'This plugins introduces a new navigation type for each topic: It should be used to place things like related links, further readups etc.';
 
 # Name of this Plugin, only used in this module
 $pluginName = 'TopicSpecificNavigationPlugin';
 our $curWeb;
 our $curTopic;
+
 # =========================
 
 sub initPlugin {
-    my( $topic, $web, $user, $installWeb ) = @_;
-    my $pluginPubHome = Foswiki::Func::getPubUrlPath()."/System/$pluginName";
-    Foswiki::Func::registerRESTHandler('getnavigation', \&_getSubnavigation);
-    Foswiki::Func::registerRESTHandler('setnavigation', \&_saveSubnavigation);
+    my ( $topic, $web, $user, $installWeb ) = @_;
+    my $pluginPubHome = Foswiki::Func::getPubUrlPath() . "/System/$pluginName";
+    Foswiki::Func::registerRESTHandler( 'getnavigation', \&_getSubnavigation );
+    Foswiki::Func::registerRESTHandler( 'setnavigation', \&_saveSubnavigation );
 
-    my $output = '<style type="text/css" media="screen">@import url(\''.$pluginPubHome.'/topicspecificnav.css\');</style>';
-    Foswiki::Func::addToHEAD($pluginName."_basecss",$output);
-    $curWeb = $web;
+    my $output =
+        '<style type="text/css" media="screen">@import url(\''
+      . $pluginPubHome
+      . '/topicspecificnav.css\');</style>';
+    Foswiki::Func::addToHEAD( $pluginName . "_basecss", $output );
+    $curWeb   = $web;
     $curTopic = $topic;
+
     # Plugin correctly initialized
     return 1;
 }
 
-sub _getSubnavigation{
-    my $session = shift;    
-    my $web = $session->{webName};
-    my $topic = $session->{topicName};  
-    
-    my %result = Foswiki::Plugins::DBConnectorPlugin::getValues($web, $topic, ['subnavigation']);    
+sub _getSubnavigation {
+    my $session = shift;
+    my $web     = $session->{webName};
+    my $topic   = $session->{topicName};
+
+    my %result =
+      Foswiki::Plugins::DBConnectorPlugin::getValues( $web, $topic,
+        ['subnavigation'] );
     return $result{'subnavigation'};
 }
 
-sub _saveSubnavigation{
-    my $session = shift;    
-    my $web = $session->{webName};
-    my $topic = $session->{topicName};  
-    my $query = $session->{cgiQuery};
-    my $subnavigation = $query->param("subnavigation");    
-    my %pairs;    
+sub _saveSubnavigation {
+    my $session       = shift;
+    my $web           = $session->{webName};
+    my $topic         = $session->{topicName};
+    my $query         = $session->{cgiQuery};
+    my $subnavigation = $query->param("subnavigation");
+    my %pairs;
     $pairs{'subnavigation'} = $subnavigation;
-    my %result = Foswiki::Plugins::DBConnectorPlugin::updateValues($web, $topic, \%pairs);
+    my %result =
+      Foswiki::Plugins::DBConnectorPlugin::updateValues( $web, $topic,
+        \%pairs );
     return 1;
 }
 
 1;
+
 # vim: ft=perl foldmethod=marker
